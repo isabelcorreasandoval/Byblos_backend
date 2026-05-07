38923852bf705e9df74a089e0e649e369b41df9c
isabelcorreasandoval <isabel.correa.sandoval@gmail.com>
2026-05-07 13:52:39 -0600

    cambio

--- controllers/contactoControllers.js
+++ controllers/contactoControllers.js
@@ -2,10 +2,12 @@ const Contacto = require('../models/ContactoModel');
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp-relay.brevo.com',
+    port: 587,
+    secure: false,
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        user: process.env.BREVO_USER,
+        pass: process.env.BREVO_PASS
     }
 });
 
